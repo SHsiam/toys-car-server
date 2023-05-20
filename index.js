074503c0ce429b5b0e29609ad58dd676bdfc3c8d
Sayed Hossain Bhuiyan <mdshiam4@gmail.com>
2023-05-21 01:54:58 +0600

    car added with id

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ async function run() {
       res.send(result);
   })
 
+  app.get('/products/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await carsCollection.findOne(query);
+    res.send(result);
+})
 
 
     // Send a ping to confirm a successful connection
